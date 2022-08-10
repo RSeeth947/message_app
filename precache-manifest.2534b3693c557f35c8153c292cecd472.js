e14d77f34967a3e7198a1b4962274c5ef3130e49
RSeeth947 <rupesh.seeth@gmail.com>
2022-08-09 20:46:20 -0400

    Updates

---
+++ precache-manifest.2534b3693c557f35c8153c292cecd472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50e0463004c842dc8178",
+    "url": "/message_app/static/css/main.816c0e92.chunk.css"
+  },
+  {
+    "revision": "50e0463004c842dc8178",
+    "url": "/message_app/static/js/main.50e04630.chunk.js"
+  },
+  {
+    "revision": "269d177dfa7b1bf5e155",
+    "url": "/message_app/static/js/1.269d177d.chunk.js"
+  },
+  {
+    "revision": "332b7d8b4eb7a74dfc0d",
+    "url": "/message_app/static/js/2.332b7d8b.chunk.js"
+  },
+  {
+    "revision": "5d7bb9f40d2e4de54e6d",
+    "url": "/message_app/static/js/runtime~main.5d7bb9f4.js"
+  },
+  {
+    "revision": "1248c20f119c0c3c1f8af3f4ae0e2268",
+    "url": "/message_app/index.html"
+  }
+];
